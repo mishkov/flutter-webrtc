ad8dcc3784cc4d064b766b969b76d579b1b5ce69
Nikita Mishkov <mishkov.dd@gmail.com>
2022-01-08 15:20:18 +0300

    Implement FrameStream mixin

--- lib/src/native/frame_stream.dart
+++ lib/src/native/frame_stream.dart
@@ -1,11 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+import '../../flutter_webrtc.dart';
 import 'media_stream_track_impl.dart';
 
 mixin FrameStream on MediaStreamTrackNative {
+  StreamSubscription<dynamic>? _frameStreamSubscription;
+
   Future<void> startFrameStream(Function(dynamic frame) onFrame) async {
-    throw UnimplementedError();
+    await WebRTC.invokeMethod(
+      'startFrameStream',
+      <String, dynamic>{'trackId': id},
+    );
+
+    final cameraEventChannel =
+        EventChannel('FlutterWebRTC.Method/frameStream/$id');
+
+    // TODO: Implement conversion to readble format
+    _frameStreamSubscription =
+        cameraEventChannel.receiveBroadcastStream().listen(onFrame);
   }
 
   Future<void> stopFrameStream() async {
-    throw UnimplementedError();
+    await WebRTC.invokeMethod(
+      'stopFrameStream',
+      <String, dynamic>{'trackId': id},
+    );
+
+    await _frameStreamSubscription?.cancel();
+    _frameStreamSubscription = null;
   }
 }
